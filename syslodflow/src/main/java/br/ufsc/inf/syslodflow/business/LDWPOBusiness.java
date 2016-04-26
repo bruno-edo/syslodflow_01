47acf581a609c4318821b6a15d49e15474c80ae6
jean <jean@inf.ufs.br>
2016-04-26 00:01:34 -0300

    Alterações instanciação da ontologia

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/business/LDWPOBusiness.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/business/LDWPOBusiness.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
@@ -14,7 +15,6 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.ontology.OntResource;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator;
@@ -22,22 +22,27 @@ import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 @Stateless
 public class LDWPOBusiness {
 	
+	@PostConstruct
 	public void init() {
+
+	}
+	
+	public OntModel doNewModel() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
 		String arquivo = scontext.getRealPath("/ontology/");
 		java.nio.file.Path pathLdwpo = Paths.get(arquivo, "ldwpo.owl");
 		
-		OntModel modelo = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-        modelo.read(pathLdwpo.toUri().toString(), ""); 
-        OntClass classe = modelo.getOntClass("http://ldwpo.aksw.org/terms/1.0/LDWProject");
-
-        Individual projeto = listIndividuals(classe).get(0);
-        Property myProperty = modelo.getProperty("http://ldwpo.aksw.org/terms/1.0/name");
-        System.out.println("name: " + projeto.getPropertyValue(myProperty).asLiteral().getString());
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        model.read(pathLdwpo.toUri().toString(), "");
+        return model;
+		
 	}
 	
-	
+//	public doSaveModel(String nameProject) {
+//		
+//	}
+
 	public static List<Individual> listIndividuals(OntClass classe) {
 
 		List<Individual> listIndividuals = new ArrayList<Individual>();
--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/enumerator/ClassURIEnum.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/enumerator/ClassURIEnum.java
@@ -1,5 +1,21 @@
 package br.ufsc.inf.syslodflow.enumerator;
 
 public enum ClassURIEnum {
+	LDWPROJECT("http://ldwpo.aksw.org/terms/1.0/LDWProject");
+	
+	private String uri;
+	
+	ClassURIEnum(String uri) {
+		this.uri = uri;
+	}
 
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	
+	
 }
