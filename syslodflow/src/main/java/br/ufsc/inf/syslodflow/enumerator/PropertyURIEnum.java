3693136571f1c034657b696680141c3310562eac
jean <jean@inf.ufs.br>
2016-04-28 21:41:41 -0300

    Adicionadas variáveis name e creator

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/enumerator/PropertyURIEnum.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/enumerator/PropertyURIEnum.java
@@ -1,5 +1,22 @@
 package br.ufsc.inf.syslodflow.enumerator;
 
 public enum PropertyURIEnum {
+	NAME("http://ldwpo.aksw.org/terms/1.0/name"),
+	CREATOR("http://ldwpo.aksw.org/terms/1.0/creator");
+	
+	private String uri;
+	
+	PropertyURIEnum(String uri) {
+		this.uri = uri;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	
 
 }
