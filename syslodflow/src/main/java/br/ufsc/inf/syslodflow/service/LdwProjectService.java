217ddf21fa443af033b64db4f605ae3b87861f24
Jhonatan <Jhonatan@acere15-jhon>
2016-05-01 23:01:25 -0300

    Criação do método getLdwProject para retorno de entidade LDWProject montada, a partir de dados persistidos na ontologia.
    Inclusão de novas Propriedades no PropertyURIEnum.
    
    Criação de novo método genérico na classe BaseService para retorno de
    valores de propriedades que são strings.

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/LdwProjectService.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/LdwProjectService.java
@@ -7,12 +7,17 @@ import java.util.List;
 import javax.inject.Inject;
 
 import br.ufsc.inf.syslodflow.dto.LDWProjectDTO;
+import br.ufsc.inf.syslodflow.entity.Homepage;
 import br.ufsc.inf.syslodflow.entity.LDWProject;
+import br.ufsc.inf.syslodflow.entity.Location;
+import br.ufsc.inf.syslodflow.entity.Person;
+import br.ufsc.inf.syslodflow.entity.Report;
 import br.ufsc.inf.syslodflow.enumerator.ClassURIEnum;
 import br.ufsc.inf.syslodflow.enumerator.PropertyURIEnum;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 public class LdwProjectService extends BaseService {
@@ -51,10 +56,48 @@ public class LdwProjectService extends BaseService {
 	public LDWProject getLdwProject(OntModel model) {
 		
 		Individual ontProject = listIndividuals(model.getOntClass(ClassURIEnum.LDWPROJECT.getUri())).get(0);
+		String ldwProjectName = getPropertyStringValue(ontProject, model, PropertyURIEnum.NAME.getUri());
+		String ldwProjectDescription = getPropertyStringValue(ontProject, model, PropertyURIEnum.DESCRIPTION.getUri());
+		String ldwProjectGoal = getPropertyStringValue(ontProject, model, PropertyURIEnum.GOAL.getUri());
 		
-		String ldwProjectName = getIndividualName(ontProject, model);
+		Individual creator = (Individual) ontProject.getPropertyValue(model.getProperty(PropertyURIEnum.CREATOR.getUri()));
+		String creatorName = getPropertyStringValue(creator, model, PropertyURIEnum.NAME.getUri());
 		
-		return new LDWProject();
+		Individual homepage = (Individual) ontProject.getPropertyValue(model.getProperty(PropertyURIEnum.HOMEPAGE.getUri()));
+		String homepageName = getPropertyStringValue(homepage, model, PropertyURIEnum.NAME.getUri());
+		Individual homepageLocation = (Individual) homepage.getPropertyValue(model.getProperty(PropertyURIEnum.LOCATION.getUri()));
+		String homepageLocationValue = getPropertyStringValue(homepageLocation, model, PropertyURIEnum.VALUE.getUri());
+		
+		Individual report = (Individual) ontProject.getPropertyValue(model.getProperty(PropertyURIEnum.REPORT.getUri()));
+		String reportName = getPropertyStringValue(report, model, PropertyURIEnum.NAME.getUri());
+		Individual reportLocation = (Individual) report.getPropertyValue(model.getProperty(PropertyURIEnum.LOCATION.getUri()));
+		String reportLocationValue = getPropertyStringValue(reportLocation, model, PropertyURIEnum.VALUE.getUri());
+	
+		Person ldwProjectCreator = new Person();
+		ldwProjectCreator.setName(creatorName);
+		
+		Homepage ldwProjectHomepage = new Homepage();
+		ldwProjectHomepage.setName(homepageName);
+		Location ldwProjectHomepageLocation = new Location();
+		ldwProjectHomepageLocation.setValue(homepageLocationValue);
+		ldwProjectHomepage.setLocation(ldwProjectHomepageLocation);
+		
+		Report ldwProjectReport = new Report();
+		ldwProjectReport.setName(reportName);
+		Location ldwProjectReportLocation = new Location();
+		ldwProjectReportLocation.setValue(reportLocationValue);
+		ldwProjectReport.setLocation(ldwProjectReportLocation);
+		
+		
+		LDWProject ldwProject = new LDWProject();
+		ldwProject.setName(ldwProjectName);
+		ldwProject.setDescription(ldwProjectDescription);
+		ldwProject.setGoal(ldwProjectGoal);
+		ldwProject.setCreator(ldwProjectCreator);
+		ldwProject.setHomePage(ldwProjectHomepage);
+		ldwProject.setReport(ldwProjectReport);
+		
+		return ldwProject;
 		
 	}
 	
