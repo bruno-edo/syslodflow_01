b132eeba0805771396a56d48f571cf30753660e8
Jhonatan <Jhonatan@acere15-jhon>
2016-05-05 21:55:56 -0300

    Finalização LDWStepService. Criação de métodos de apoio. Ajuste nas entidades. Criação de novas propriedades no Enum de Propriedades.

---
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/LdwStepExecutionService.java
@@ -0,0 +1,15 @@
+package br.ufsc.inf.syslodflow.service;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+
+import br.ufsc.inf.syslodflow.entity.LDWStepExecution;
+
+public class LdwStepExecutionService extends BaseService {
+	
+	public LDWStepExecution getLdwStepExecution(OntModel model, Individual ontLdwStepExec) {
+		
+		return null;
+	}
+
+}
