c2b27d40bba53537addb9dba4c0f8255a9bb31e4
Jhonatan <Jhonatan@acere15-jhon>
2016-05-16 23:32:23 -0300

    Criação de Namespace Enum, Implementação de lógica de inserção de dados de projeto.

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/LdwProjectService.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/LdwProjectService.java
@@ -17,6 +17,7 @@ import br.ufsc.inf.syslodflow.enumerator.ClassURIEnum;
 import br.ufsc.inf.syslodflow.enumerator.PropertyURIEnum;
 
 import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Resource;
 
@@ -112,5 +113,95 @@ public class LdwProjectService extends BaseService {
 		return model;
 	}
 	
+	public void writeLdwProject(OntModel model, LDWProject project) {
+		
+		if (URIalreadyExists(model, project.getUri()))
+			editLdwProject(model, project);
+		else 
+			insertLdwProject(model, project);
+	}
+	
+	private void editLdwProject(OntModel model, LDWProject project) {
+		
+	Individual ldwProject = model.getIndividual(project.getUri());
+	ldwProject.removeAll(model.getProperty(PropertyURIEnum.NAME.getUri()));
+	ldwProject.removeAll(model.getProperty(PropertyURIEnum.GOAL.getUri()));
+	ldwProject.removeAll(model.getProperty(PropertyURIEnum.DESCRIPTION.getUri()));
+	
+	ldwProject.addLiteral(model.getProperty(PropertyURIEnum.NAME.getUri()), project.getName());
+	ldwProject.addLiteral(model.getProperty(PropertyURIEnum.GOAL.getUri()), project.getGoal());
+	ldwProject.addLiteral(model.getProperty(PropertyURIEnum.DESCRIPTION.getUri()), project.getDescription());
+	
+	// Verifica existência de Criador
+	if(URIalreadyExists(model, project.getCreator().getUri())) {
+		Individual ldwProjectCreator = model.getIndividual(ldwProject.getPropertyResourceValue(model.getProperty(PropertyURIEnum.CREATOR.getUri())).getURI());
+		ldwProject.removeAll(model.getProperty(PropertyURIEnum.CREATOR.getUri()));
+		ldwProject.addProperty(model.getProperty(PropertyURIEnum.CREATOR.getUri()), ldwProjectCreator);
+	}
+	else {
+		Individual ldwProjectCreator = model.getOntClass(ClassURIEnum.PERSON.getUri()).createIndividual(project.getCreator().getUri());
+		ldwProjectCreator.addLiteral(model.getProperty(PropertyURIEnum.NAME.getUri()), project.getCreator().getName());
+		ldwProject.addProperty(model.getProperty(PropertyURIEnum.CREATOR.getUri()), ldwProjectCreator);
+	}
+	
+	// Verifica existência de Homepage
+	if(URIalreadyExists(model, project.getHomePage().getUri())) {
+		Individual ldwProjectHomepage = model.getIndividual(ldwProject.getPropertyResourceValue(model.getProperty(PropertyURIEnum.HOMEPAGE.getUri())).getURI());
+		Individual ldwProjectHomepageLocation = model.getIndividual(ldwProjectHomepage.getPropertyResourceValue(model.getProperty(PropertyURIEnum.LOCATION.getUri())).getURI());
+		ldwProjectHomepageLocation.removeAll(model.getProperty(PropertyURIEnum.VALUE.getUri()));
+		ldwProjectHomepageLocation.addLiteral(model.getProperty(PropertyURIEnum.VALUE.getUri()), project.getHomePage().getLocation().getValue());
+	}
+	else {
+		Individual ldwProjectHomepage = model.getOntClass(ClassURIEnum.HOMEPAGE.getUri()).createIndividual(project.getHomePage().getUri());
+		ldwProjectHomepage.addLiteral(model.getProperty(PropertyURIEnum.NAME.getUri()), project.getHomePage().getName());
+		Individual ldwProjectHomepageLocation = model.getOntClass(ClassURIEnum.LOCATION.getUri()).createIndividual(project.getHomePage().getLocation().getUri());
+		ldwProjectHomepageLocation.addLiteral(model.getProperty(PropertyURIEnum.VALUE.getUri()), project.getHomePage().getLocation().getValue());
+		ldwProjectHomepage.addProperty(model.getProperty(PropertyURIEnum.LOCATION.getUri()), ldwProjectHomepageLocation);
+		ldwProject.addProperty(model.getProperty(PropertyURIEnum.HOMEPAGE.getUri()), ldwProjectHomepage);
+	}
+	
+	// Verifica existência de Report
+	
+	if(URIalreadyExists(model, project.getReport().getUri())) {
+		Individual ldwProjectReport = model.getIndividual(ldwProject.getPropertyResourceValue(model.getProperty(PropertyURIEnum.REPORT.getUri())).getURI());
+		ldwProjectReport.removeAll(model.getProperty(PropertyURIEnum.NAME.getUri()));
+		Individual ldwProjectReportLocation = model.getIndividual(ldwProjectReport.getPropertyResourceValue(model.getProperty(PropertyURIEnum.LOCATION.getUri())).getURI());
+		ldwProjectReportLocation.removeAll(model.getProperty(PropertyURIEnum.VALUE.getUri()));
+		ldwProject.addLiteral(model.getProperty(PropertyURIEnum.NAME.getUri()), project.getReport().getName());
+		ldwProjectReportLocation.addLiteral(model.getProperty(PropertyURIEnum.VALUE.getUri()), project.getReport().getLocation().getValue());
+		
+	}
+	else
+	{
+		Individual ldwProjectReport = model.getOntClass(ClassURIEnum.REPORT.getUri()).createIndividual(project.getReport().getUri());
+		ldwProjectReport.addLiteral(model.getProperty(PropertyURIEnum.NAME.getUri()), project.getReport().getName());
+		Individual ldwProjectReportLocation = model.getOntClass(ClassURIEnum.LOCATION.getUri()).createIndividual(project.getReport().getLocation().getUri());
+		ldwProjectReportLocation.addLiteral(model.getProperty(PropertyURIEnum.VALUE.getUri()), project.getReport().getLocation().getValue());
+		ldwProjectReport.addProperty(model.getProperty(PropertyURIEnum.LOCATION.getUri()), ldwProjectReportLocation);
+		ldwProject.addProperty(model.getProperty(PropertyURIEnum.REPORT.getUri()), ldwProjectReport);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//recuperar objeto
+	// deletar valores de propriedades ja contidas
+	// inserir novos valores de propriedades
+	
+		
+	}
 
+	private void insertLdwProject(OntModel model, LDWProject project) {
+		
+	}
+	
+	
 }
