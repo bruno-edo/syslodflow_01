5dac00f7f140abc8475e0a54673ada5256a2d337
jean <jean@inf.ufs.br>
2016-05-29 16:06:12 -0300

    Converter para entidade Person

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/entity/Person.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/entity/Person.java
@@ -1,6 +1,10 @@
 package br.ufsc.inf.syslodflow.entity;
 
-public class Person extends BaseModel {
+import java.io.Serializable;
+
+public class Person extends BaseModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	private String name;
 	private String uri;
@@ -31,6 +35,38 @@ public class Person extends BaseModel {
 	public void setUri(String uri) {
 		this.uri = uri;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
