1af3be7944544cd33372f1ce04d3360dad9b57fe
jeanmorais <jeanmorais>
2016-06-21 19:56:55 -0300

    Alterações step 02

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/CommandService.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/CommandService.java
@@ -5,22 +5,36 @@ import javax.faces.context.FacesContext;
 public class CommandService {
 	
 	
-	public String createCommandStep02(String ldwProjectName) {
+	public String createScriptStep02(String ldwProjectName) {
 		FacesContext fc = FacesContext.getCurrentInstance();
 		String filePath = fc.getExternalContext().getInitParameter("projectsPath").toString();
 		filePath = filePath + "/" + ldwProjectName ;
+		
+		String ntPath = filePath + "/nt/";
+		String ntDataset = filePath + "/nt/dataset.nt";
+		String ntProject = filePath + "/" + ldwProjectName + ".nt";
+		String csvDataset = filePath + "/csv/dataset.csv";
+		String mappingSml = filePath + "/mappings/mapping.sml";
 		
 		/**
 		 * Comando que será escrito no disco
 		 */
 		StringBuilder sb = new StringBuilder();
-		sb.append("sparqlify-csv -f ");
-		sb.append(filePath + "/csv/dataset.csv " +  "-c" + filePath + "/mappings/mapping.sml");
-		sb.append(" > " + filePath + "/nt/dataset.nt");
+		
+		sb.append("mkdir -p " + ntPath);
+		
+		sb.append("if [ -f " + ntDataset + " ]; then rm " + ntDataset + "; fi ");
+		sb.append("if [ -f " + ntProject + " ]; then rm " + ntProject + "; fi ");
+		
+		sb.append("sparqlify-csv -f " + csvDataset +  "-c" + mappingSml + " > " + ntDataset);
+	
+		
+		
 		
 		return "";
-		
 	}
+	
+	
 	
 
 }
