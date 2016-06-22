0fdb69130fbb8ab05701f54d0fe33fcfc6a27087
jeanmorais <jeanmorais>
2016-06-22 20:00:30 -0300

    #CHANGE Alterações commandservice

--- syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/CommandService.java
+++ syslodflow/src/main/java/br/ufsc/inf/syslodflow/service/CommandService.java
@@ -1,6 +1,13 @@
 package br.ufsc.inf.syslodflow.service;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.faces.context.FacesContext;
+
+import br.ufsc.inf.syslodflow.util.MessageUtil;
 
 public class CommandService {
 	
@@ -15,6 +22,8 @@ public class CommandService {
 		String ntProject = filePath + "/" + ldwProjectName + ".nt";
 		String csvDataset = filePath + "/csv/dataset.csv";
 		String mappingSml = filePath + "/mappings/mapping.sml";
+		String binPath = filePath + "/path/";
+		String applyingSparqlify = "applyingSparqlify.sh";
 		
 		/**
 		 * Comando que será escrito no disco
@@ -29,7 +38,20 @@ public class CommandService {
 		sb.append("sparqlify-csv -f " + csvDataset +  "-c" + mappingSml + " > " + ntDataset);
 	
 		
-		
+		try {
+			File targetFolder = new File(binPath);
+			if (!targetFolder.exists()) {
+				targetFolder.mkdirs();
+			} 
+			FileWriter arq = new FileWriter(binPath + applyingSparqlify);
+		    PrintWriter gravarArq = new PrintWriter(arq);
+		    gravarArq.printf(sb.toString());
+		    gravarArq.close();
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
+			return MessageUtil.getMessageBundle("crud.file.saveerror");
+		}
 		
 		return "";
 	}
